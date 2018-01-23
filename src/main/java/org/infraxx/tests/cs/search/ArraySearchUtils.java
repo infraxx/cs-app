534ed501d95a5cb7278f0b6d2bda48b4a98936ad
Igor Suchkov <infraxx@gmail.com>
2018-01-23 15:55:09 +0200

    bsearch

--- src/main/java/org/infraxx/tests/cs/search/ArraySearchUtils.java
+++ src/main/java/org/infraxx/tests/cs/search/ArraySearchUtils.java
@@ -1,10 +1,7 @@
 package org.infraxx.tests.cs.search;
 
 import java.io.Console;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class ArraySearchUtils {
@@ -60,8 +57,56 @@ public class ArraySearchUtils {
         return expectant;
     }
 
-    public static void main(String[] argv) {
+    // Location of found element
+    public static int binarySearchLoop(int[] array, int key) {
+
+        int first = 0;
+        int last = array.length;
+
+        while (first <= last) {
+            System.out.println(first + " " + last);
+            int middle = (first + last )/2;
+
+            if (array[middle] > key) {
+                System.out.println(">");
+                last = middle;
+            }
+            else if (array[middle] < key) {
+                System.out.println("<");
+                first = middle;
+            }
+            else {
+                return middle;
+            }
+               
+        }
+
+        return -1;
+    }
+
+    public static int binarySearchRec(int[] array, int from, int to, int key) {
+        if (array.length == 0) {
+            return -1;
+        }
+
+        int middle = (from + to)/2;
+        if (array[middle] < key) {
+            return binarySearchRec(array, middle, to, key);
+        }
+        else if (array[middle] > key) {
+            return binarySearchRec(array, from, middle, key);
+        }
+        else {
+            return middle;
+        }
+    }
+
+    public static void main(String[] args) {
         //System.out.println(ArraySearchUtils.findMax(new int[] {1, 3, 44, 0, -1, 44}));
-        System.out.println(ArraySearchUtils.findElementWithMaxOccurences(new int[] {1, 3, 44, 0, -1, 44, 1, 1, 0, 0, 5, 7, 11, 0, 1, -1}));
+        //System.out.println(ArraySearchUtils.findElementWithMaxOccurences(new int[] {1, 3, 44, 0, -1, 44, 1, 1, 0, 0, 5, 7, 11, 0, 1, -1}));
+
+//        System.out.println(binarySearchRec(new int[] {1, 2, 3, 4, 5, 10, 15, 30}, 0, 8, 20));
+        System.out.println(binarySearchLoop(new int[] {1, 2, 3, 4, 5, 10, 15, 30}, 20));
     }
 }
+
